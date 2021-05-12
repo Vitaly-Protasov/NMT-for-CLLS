bbc1020831f071cb545d4bf348800c5770747e96
Vitaly Protasov <izakladno@yandex.ru>
2021-05-12 12:10:40 +0300

    fixed scripts and reqs

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ python setup.py install
 cd ../
 git clone https://github.com/andreabac3/Word-Alignment-BERT.git
 cp -r Word-Alignment-BERT/WordAlignment.py .
+rm -rf Word-Alignment-BERT/ awesome-align/
