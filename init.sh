428742dad786513e745bd28ea27f7d8bc4e23b6a
Vitaly Protasov <izakladno@yandex.ru>
2021-05-12 11:58:28 +0300

    fixed script

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/neulab/awesome-align.git
 python -m spacy download es
+cd awesome-align/
 python setup.py install
+cd ../
 git clone https://github.com/andreabac3/Word-Alignment-BERT.git
 cp -r Word-Alignment-BERT/WordAlignment.py .
