e17f4b70973fbdb0a675e276798dfb4418767e61
Vitaly Protasov <izakladno@yandex.ru>
2021-05-12 15:00:18 +0300

    simplified script

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
-git clone https://github.com/neulab/awesome-align.git
+pip install git+https://github.com/neulab/awesome-align.git
 python -m spacy download es
-cd awesome-align/
-python setup.py install
-cd ../
 git clone https://github.com/andreabac3/Word-Alignment-BERT.git
 cp -r Word-Alignment-BERT/WordAlignment.py .
-rm -rf Word-Alignment-BERT/ awesome-align/
+rm -rf Word-Alignment-BERT/
